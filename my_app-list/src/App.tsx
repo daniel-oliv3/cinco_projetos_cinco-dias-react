5962840648d514a9ac58f93cea8116225e1d6711
Daniel Oliveira <danielsapup3@gmail.com>
2021-10-05 17:13:21 -0300

    Components ListItem

--- my_app-list/src/App.tsx
+++ my_app-list/src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from 'react';
 import * as C from './App.styles';
+import { Item } from '../src/types/item';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar uma cerveja no buteco.', done: false },
+    { id: 2, name: 'Comprar uma ficha de fliperama.', done: false },
+  ]);
+
   return(
       <C.Container>
-          ....
+        <C.Area>
+          <C.Header>Lista de Tarefas</C.Header>
+
+          {list.map((item, index)=>(
+            <div>{item.name}</div>
+          ))}
+        </C.Area>
       </C.Container>  
   );
 }
