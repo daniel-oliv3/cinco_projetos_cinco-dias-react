d46bab3f260b6092739ec709b70aa74f2a7b7ac6
Daniel Oliveira <danielsapup3@gmail.com>
2021-10-06 08:25:38 -0300

    Update

--- my_app-list/src/components/AddArea/index.tsx
+++ my_app-list/src/components/AddArea/index.tsx
@@ -3,7 +3,11 @@ import * as C from './styles';
 export const AddArea = () => {
     return (
         <C.Container>
-            ...
+            <div className="image">➕</div>
+            <input 
+                type="text"
+                placeholder="Adicione uma tarefa"
+            />
         </C.Container>
     );
 }
--- my_app-list/src/components/AddArea/styles.ts
+++ my_app-list/src/components/AddArea/styles.ts
@@ -1,3 +1,10 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+    border: 1px solid #555;
+    border-radius: 15px;
+    padding: 10px;
+    margin: 20px 0;
+    display: flex;
+    align-items: center;
+`;
