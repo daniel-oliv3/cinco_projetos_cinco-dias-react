1cc6d5d4d9910aefde2dde7a7ac1c2c4d47b3fa4
Daniel Oliveira <danielsapup3@gmail.com>
2021-10-06 09:42:40 -0300

    Adicionando novos itens a lista

--- my_app-list/src/App.tsx
+++ my_app-list/src/App.tsx
@@ -10,12 +10,22 @@ const App = () => {
     { id: 2, name: 'Comprar uma ficha de fliperama.', done: true },
   ]);
 
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+
   return(
       <C.Container>
         <C.Area>
           <C.Header>Lista de Tarefas</C.Header>
 
-          <AddArea />
+          <AddArea onEnter={handleAddTask}/>
 
           {list.map((item, index)=>(
             <ListItem key={index} item={item}/>
--- my_app-list/src/components/AddArea/styles.ts
+++ my_app-list/src/components/AddArea/styles.ts
@@ -7,4 +7,17 @@ export const Container = styled.div`
     margin: 20px 0;
     display: flex;
     align-items: center;
+
+    .image {
+        margin-right: 5px;
+    }
+
+    input {
+        border: 0px;
+        background: transparent;
+        outline: 0;
+        color: #FFF;
+        font-size: 18px;
+        flex: 1;
+    }
 `;
