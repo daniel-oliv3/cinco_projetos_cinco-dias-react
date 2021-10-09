23b3db4430f5e89ef502d4bd4f297ccc2465e7e7
Daniel Oliveira <danielsapup3@gmail.com>
2021-10-08 23:18:01 -0300

    Env

--- galeria_img/src/libs/firebase.ts
+++ galeria_img/src/libs/firebase.ts
@@ -1,8 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getStorage } from 'firebase/storage';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyCgSW41DQ5nWV_ghfoWW3178KhkldYVU-s",
-    authDomain: "d5reactgallery-befb2.firebaseapp.com",
-    projectId: "d5reactgallery-befb2",
-    storageBucket: "d5reactgallery-befb2.appspot.com",
-    messagingSenderId: "617733915113",
-    appId: "1:617733915113:web:a99dfd27524be339810b78"
+    apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSEAGINGSENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APPID
   };
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  export const storage = getStorage(firebaseApp);
