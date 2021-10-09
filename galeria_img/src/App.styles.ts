51d417c6beb8e05c0dbc413da9fd8f77e4f08415
Daniel Oliveira <danielsapup3@gmail.com>
2021-10-09 00:33:33 -0300

    Header

---
+++ galeria_img/src/App.styles.ts
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #27282F;
+    color: #FFF;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 30px 0;
+`;
+
+export const Header = styled.h1`
+    margin: 0;
+    padding: 0;
+    text-align: center;
+    margin-bottom: 30px;
+`;
--- galeria_img/src/App.tsx
+++ galeria_img/src/App.tsx
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import * as C from './App.styles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        {/* Área de Uploud */}
+
+        {/* Lista de Fotos */}
+      </C.Area>
+    </C.Container>
   );
 }
 
